6472e2f7c81a0dcbf1d124881e281a5a12bc2b6a
Mazen Saleh <mazenksaleh1@gmail.com>
2025-06-07 14:11:43 -0400

    Fix: correct __main__ syntax

--- studybot.py
+++ studybot.py
@@ -111,6 +111,6 @@ def sms_reply():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
